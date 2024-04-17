dd38179ae8f1d557772e4649baa1d9546299df21
luis99leiva <luis99leiva@gmail.com>
2024-04-17 13:41:58 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar.tsx";
+import Header from "./components/Header.tsx";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
+
+// {
+//   fontFamily: roboto: ["Roboto", "sans-serif"]
+// },
+// backgroundImage: {
+//   'placeholder-image': "url('/assets/jaguar1.png')"
+// }
