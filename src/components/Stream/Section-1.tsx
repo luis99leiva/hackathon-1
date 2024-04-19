99f5ae01ad07b15a2485398a824162dd99ebba0f
luis99leiva <luis99leiva@gmail.com>
2024-04-19 14:19:26 -0400

    14:19

--- src/components/Stream/Section-1.tsx
+++ src/components/Stream/Section-1.tsx
@@ -1,47 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Section1 = ({ className = "" }) => {
+  const videos = ["../assets/video1.mp4", "../assets/video2.mp4", "../assets/video3.mp4"];
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
+  const handleButtonClick = (index) => {
+    console.log("Button clicked with index:", index);
+    setCurrentVideoIndex(index);
+  };
+
+  console.log("Current video index:", currentVideoIndex);
+
   return (
     <div className={`font-roboto inline-flex w-full flex-col items-start gap-y-5 bg-white px-16 pb-1 pt-28 tracking-[0px] text-black ${className}`}>
       {/* Main content */}
-      <div className="flex items-center self-stretch text-left text-4xl font-bold leading-tight">
-        <p>Experimenta la emoción en tiempo real con nuestras transmisiones interactivas</p>
-      </div>
-      {/* Play button */}
-      <button className="mt-14 flex items-center justify-center self-stretch p-72 bg-gradient">
-        <div className="bg-PlayButtonIcon flex items-center justify-center bg-cover bg-center px-16 py-12">  
-        </div>
-          <video width="2000" height="1500" controls >
-          <source src="../assets/yaguarete-comiendo.mp4" type="video/mp4"/>
-          </video>
-      </button>
-      <div className="bg-Frame3Icon flex items-center justify-end self-stretch px-1">
+      <div className="flex-row items-center self-stretch text-left text-4xl font-bold leading-tight">
+        <p className='mb-10 '>Experimenta la emoción en tiempo real con nuestras transmisiones interactivas</p>
+        <video width="2000" height="1500" controls>
+          <source src={videos[currentVideoIndex]} type="video/mp4"/>
+        </video>
       </div>
       {/* Price tags */}
       <div className="flex flex-wrap items-center justify-center gap-x-80 gap-y-12 self-stretch pt-7 text-center text-base font-normal leading-normal min-w-[1430px]:flex-nowrap">
         <div className="flex w-56 flex-shrink-0 flex-col items-center justify-center gap-y-2 self-stretch rounded-xl border border-solid border-black bg-white p-3">
-          <button className="button-primary" >
+          <button className="button-primary" onClick={() => handleButtonClick(0)}>
             <img src="../assets/water-drop.png" alt="water droplet" className="h-8 w-8 flex-shrink-0" />
               <p style={{ marginTop: '0.5rem' }}>$1</p>
           </button>
-         </div>
-         <div className="flex w-56 flex-shrink-0 flex-col items-center justify-center gap-y-2 self-stretch rounded-xl border border-solid border-black bg-white p-3">
-          <button className="button-primary" >
-          <img src="../assets/night-vision-camera.png" alt="night vision" className="h-8 w-8 flex-shrink-0" />
+        </div>
+        <div className="flex w-56 flex-shrink-0 flex-col items-center justify-center gap-y-2 self-stretch rounded-xl border border-solid border-black bg-white p-3">
+          <button className="button-primary" onClick={() => handleButtonClick(1)}>
+            <img src="../assets/night-vision-camera.png" alt="night vision" className="h-8 w-8 flex-shrink-0" />
             <p style={{ marginTop: '0.5rem' }}>$3</p>
           </button>
         </div>
         <div className="flex w-56 flex-shrink-0 flex-col items-center justify-center gap-y-2 self-stretch rounded-xl border border-solid border-black bg-white p-3">
-          <button className="button-primary" >
-          <img src="../assets/chicken-leg.png" alt="chicken leg" className="h-8 w-8 flex-shrink-0" />
+          <button className="button-primary" onClick={() => handleButtonClick(2)}>
+            <img src="../assets/chicken-leg.png" alt="chicken leg" className="h-8 w-8 flex-shrink-0" />
             <p style={{ marginTop: '0.5rem' }}>$5</p>
           </button>
         </div>
       </div>
     </div>
-    
   );
 }
 
 export default Section1;
-
